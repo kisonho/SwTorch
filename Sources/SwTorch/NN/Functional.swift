fadbc34b271d9c99960d4151f91408fbe368b19c
Qisheng He <RobertHe.KsHo@wayne.edu>
2021-11-29 15:09:52 -0500

    11-29-21
    - logSoftmax implementation

--- Sources/SwTorch/NN/Functional.swift
+++ Sources/SwTorch/NN/Functional.swift
@@ -10,6 +10,10 @@ import PythonKit
 /// core python library
 fileprivate let F = Python.import("torch.nn.functional")
 
+public func logSoftmax(_ input: Tensor, dim: Int = 0) -> Tensor {
+    return Tensor(F.log_softmax(input, dim: dim))
+}
+
 /// Softmax function
 /// - Parameters:
 ///   - input: input `Tensor`
